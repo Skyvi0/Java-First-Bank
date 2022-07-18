d81a4282314d6740c5152fcfb1ed328894fcc0e1
vitaliy <vitaliy.skoryy@googlemail.com>
2022-07-18 19:00:38 +0200

    Add deposit function

--- Account.java
+++ Account.java
@@ -6,10 +6,18 @@ public class Account {
   int previousTransaction;
   String customerName;
   String customerID;
-}
+
 
 //Class constructor
 Account(String cname, String cid) {
   customerName = cname;
   customerID = cid;
+}
+
+void deposit(int amount) {
+  if (amount != 0) {
+    balance = balance + amount;
+    previousTransaction = amount;
+  }
+
 }
