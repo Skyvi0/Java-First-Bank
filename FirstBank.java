dbddf9cd4a8c02258130349ec8e246adaba2ae49
vitaliy <vitaliy.skoryy@googlemail.com>
2022-07-18 18:52:02 +0200

    first Account changes

--- FirstBank.java
+++ FirstBank.java
@@ -1,7 +1,7 @@
 public class FirstBank {
 
   public static void main(String[] args) {
-    Account bob = new Account("Bob", "A00001");
+    Account bob = new Account("Bob", "ACCOUNT001");
     bob.showMenu();
   }
 }
